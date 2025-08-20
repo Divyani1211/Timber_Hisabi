f9736e7e2b26de064e69eb831bb608f40b383e5f
Nayka Divyani <ddivyaninayka@gmail.com>
2025-08-20 20:04:28 +0530

    Delete django_project/django/timber_hisabi directory

--- django_project/django/timber_hisabi/home/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from.import views
-urlpatterns=[
-    path('',views.home_page,name='home'),
-     path('', views.login_view, name="login"),
-    path('signup/', views.signup_view, name="signup"),
-    path('home/', views.home_view, name="home"),
-]
